dcdd5b880c833f865326eda3e902184d53bfdb1e
Suraj <suraj@masterblocks.co.in>
2024-03-29 16:10:26 +0530

    1. Routing name changed to valid names 2. README file is added

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -6,9 +6,9 @@ const taskController = require('../controllers/taskController');
 
 router.get('/', taskController.getAllProducts);
 router.post('/product', taskController.createProduct);
-router.get('/:id', taskController.getProductById);
-router.put('/:id', taskController.updateProduct);
-router.delete('/:id', taskController.deleteProduct);
+router.get('/product/:id', taskController.getProductById);
+router.put('/product/:id', taskController.updateProduct);
+router.delete('/product/:id', taskController.deleteProduct);
 
 module.exports = router;
 
