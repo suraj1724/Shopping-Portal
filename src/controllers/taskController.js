59409fa6311377e465d68e17af1bed97d1adaa7c
Suraj <suraj@masterblocks.co.in>
2024-03-29 11:57:55 +0530

    Initial commit

---
+++ src/controllers/taskController.js
@@ -0,0 +1,14 @@
+// controllers/taskController.js
+
+const Task = require('../models/Task');
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Implement other controller functions (createTask, getTaskById, updateTask, deleteTask) similarly
---
+++ src/index.js
@@ -0,0 +1,22 @@
+// src/index.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/tasks', taskRoutes);
+
+mongoose.connect('mongodb://localhost:27017/shopping_portal', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch(err => console.error('Error connecting to MongoDB:', err));
